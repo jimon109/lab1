967934089e5cc318feab85eb3f5b99dc20b5e297
jimon109 <mosjimon@gmail.com>
2022-07-01 15:18:10 +0700

    3 nd

--- src/P10/Human.java
+++ src/P10/Human.java
@@ -12,10 +12,13 @@ public class Human {
     public static void main(String[] args) {
         Human jimon = new Human("Warawut",20);
         Human earn = new Human("Thidatrip",20);
+        Human nook = new Human("sakchai",20);
         System.out.println("Human Nane is" + jimon.name);
         System.out.println("Human age is" + jimon.age);
         System.out.println("Human Nane is" + earn.name);
         System.out.println("Human age is" + earn.age);
+        System.out.println("Human name is" + nook.name);
+        System.out.println("Human age is" + nook.age);
     }
 
 
